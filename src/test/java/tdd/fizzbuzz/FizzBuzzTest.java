04331c7decc26876b97df374a59f4140e7d206c6
kwokth4 <kwokth4@oocl.com>
2022-12-01 14:29:27 +0800

    feat: test method should_say_the_number_when_count_off_given_number done

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,18 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_say_the_number_when_count_off_given_number(){
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String actual = fizzBuzz.countOff(1);
+        //then
+        assertEquals("1",actual);
+    }
+
 }
