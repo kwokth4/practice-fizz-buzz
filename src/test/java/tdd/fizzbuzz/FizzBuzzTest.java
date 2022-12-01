f126c68d5db67c5032af7809aa426078b8b70391
kwokth4 <kwokth4@oocl.com>
2022-12-01 14:34:09 +0800

    feat: test method shoud_say_Fizz_when_count_off_given_mutiple_of_3 done

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -14,5 +14,11 @@ public class FizzBuzzTest {
         //then
         assertEquals("1",actual);
     }
+    @Test
+    void shoud_say_Fizz_when_count_off_given_mutiple_of_3(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String actual = fizzBuzz.countOff(3);
+        assertEquals("Fizz",actual);
+    }
 
 }
